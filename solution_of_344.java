c6725186a77f83d16af5b22371e7bf045765b773
otakusan001 <110317176+otakusan001@users.noreply.github.com>
2023-09-22 16:54:37 +0800

    Create solution_of_344.java

---
+++ solution_of_344.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int right = s.length - 1;
+        int left = 0;
+        char temp;
+        while(left < right){
+            temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;right--;
+        }
+        //return s;
+    }
+}
