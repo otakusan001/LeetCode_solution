d319dfbd34c77b72a7dc72b309519a64c88c44e7
otakusan001 <110317176+otakusan001@users.noreply.github.com>
2024-03-06 22:42:50 +0800

    Update solution_of_145.java

--- solution_of_145.java
+++ solution_of_145.java
@@ -17,7 +17,7 @@
 
 import java.util.ArrayList;
 import java.util.Collections;
-
+//递归解法
 class Solution {
     ArrayList<Integer> result = new ArrayList<Integer>();
     public List<Integer> postorderTraversal(TreeNode root) {
@@ -34,3 +34,32 @@ class Solution {
     return new_list;    
     }
 }
+
+/*
+import java.utils.Stack
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        Stack<TreeNode> st = new Stack<>();
+        if(root != null){
+            st.push(root);
+            while(!st.empty()){
+                TreeNode temp = st.peek();
+                st.pop();
+                if(temp != null){
+                    result.add(temp.val);
+                    st.push(temp.left);
+                    st.push(temp.right);
+                }
+            }
+
+        }
+        ArrayList<Integer> new_list = new ArrayList<Integer>();
+        for(Integer n : result){
+            new_list.add(n);
+        }
+        Collections.reverse(new_list);
+        return new_list;     
+    }
+}
+*/
