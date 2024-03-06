2af4e56909759fe052d734202a7e057e3134ade7
otakusan001 <110317176+otakusan001@users.noreply.github.com>
2024-03-06 22:37:16 +0800

    Update solution_of_144.java

--- solution_of_144.java
+++ solution_of_144.java
@@ -15,7 +15,7 @@
  */
 
 import java.util.ArrayList;
-
+//递归解法
 class Solution {
     ArrayList<Integer> result = new ArrayList<Integer>();
     public List<Integer> preorderTraversal(TreeNode root) {
@@ -27,3 +27,27 @@ class Solution {
     return result;    
     }
 }
+
+/*迭代法
+import java.utils.Stack
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        Stack<TreeNode> st = new Stack<>();
+        if(root != null){
+            st.push(root);
+            while(!st.empty()){
+                TreeNode temp = st.peek();
+                st.pop();
+                if(temp != null){
+                    result.add(temp.val);
+                    st.push(temp.right);
+                    st.push(temp.left);
+                }
+            }
+
+        }
+        return result;    
+    }
+}
+*/
