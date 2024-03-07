2ee89d8b9add071fbd9e12e06e01808b866b8981
otakusan001 <110317176+otakusan001@users.noreply.github.com>
2024-03-07 11:45:11 +0800

    Update solution_of_94.java

--- solution_of_94.java
+++ solution_of_94.java
@@ -14,7 +14,7 @@
  * }
  */
 import java.util.ArrayList;
-
+//递归解法
 class Solution {
     ArrayList<Integer> result = new ArrayList<Integer>();
     public List<Integer> inorderTraversal(TreeNode root) {
@@ -28,3 +28,29 @@ class Solution {
     return result;    
     }
 }
+
+/*迭代法
+import java.util.Stack;
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode cur;
+        if(root != null){
+            cur = root;
+            while(cur != null || !st.empty()){
+                if(cur != null){
+                    st.push(cur);
+                    cur = cur.left;
+                }
+                else{
+                    cur = st.pop();
+                    result.add(cur.val);
+                    cur = cur.right;
+                }
+            }
+        }
+        return result;    
+    }
+}
+*/
